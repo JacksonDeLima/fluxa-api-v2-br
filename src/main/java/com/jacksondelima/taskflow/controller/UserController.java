a0fe352ef3c45b2ef4b288b442370d9d11a30d19
JacksonDeLima <jacksonluis444@gmail.com>
2026-03-24 20:16:09 -0300

    Adicionado controlador UserController com endpoint para obter informações do usuário autenticado

---
+++ src/main/java/com/jacksondelima/taskflow/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.jacksondelima.taskflow.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/me")
+    public String getMe() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+}
