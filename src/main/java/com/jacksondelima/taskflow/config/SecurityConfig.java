45b45d46893cd192f7c48d4bfa55dbf5486809d2
JacksonDeLima <jacksonluis444@gmail.com>
2026-03-24 21:24:49 -0300

    Adicionado permissões para acesso ao Swagger e atualizado caminho no application.properties

--- src/main/java/com/jacksondelima/taskflow/config/SecurityConfig.java
+++ src/main/java/com/jacksondelima/taskflow/config/SecurityConfig.java
@@ -28,7 +28,12 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/auth/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session ->
